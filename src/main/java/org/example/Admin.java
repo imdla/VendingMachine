2f987166e5a3016d62f0f915515562dd3372932b
imdla <imdla918@gmail.com>
2024-12-23 23:26:47 +0900

    [modify] Admin 변수 및 생성자 추가

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -1,11 +1,16 @@
 package org.example;
 
 public class Admin extends User implements ProductManagable {
+    protected VendingMachine vendingMachine;
+
+    public Admin(VendingMachine vendingMachine) {
+        this.vendingMachine = vendingMachine;
+    }
 
     // 재고 관리
     @Override
     public void stockManagement() {
-            
+        
     }
 
     // 매출 확인
