ef6af47beaa14c683c4d0565a42c632bb56b652a
imdla <imdla918@gmail.com>
2024-12-23 22:38:46 +0900

    [modify] Beverage 생성자 수정

--- src/main/java/org/example/Beverage.java
+++ src/main/java/org/example/Beverage.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Beverage extends Product {
-    public Beverage(String name, int price, int stock, Type type) {
-        super(name, price, stock, type);
+
+    public Beverage(String name, int price, int stock) {
+        super(name, price, stock, Type.BEVERAGE);
     }
 }
