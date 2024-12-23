1d43897f4247ce2528a30b9d6445784f3f4bfc9e
imdla <imdla918@gmail.com>
2024-12-23 22:36:00 +0900

    [modify] Product 변수 및 생성자 추가

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,5 +1,20 @@
 package org.example;
 
 public abstract class Product {
-    
+    protected String name;
+    protected int price;
+    protected int stock;
+    protected Type type;
+
+    protected enum Type {
+        BEVERAGE,
+        ETC;
+    }
+
+    public Product(String name, int price, int stock, Type type) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.type = type;
+    }
 }
