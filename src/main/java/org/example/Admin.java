908b44d1292d83e3670308e534ad9cad76f2a011
imdla <imdla918@gmail.com>
2024-12-23 23:17:54 +0900

    [modify] Admin implements ProductManagable

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -1,9 +1,23 @@
 package org.example;
 
-public class Admin extends User {
+public class Admin extends User implements ProductManagable {
+
     // 재고 관리
+    @Override
+    public void stockManagement() {
+            
+    }
 
     // 매출 확인
+    @Override
+    public void showSales() {
+
+    }
 
     // 상품 가격 설정
+    @Override
+    public void setProductPrice(int price) {
+
+    }
+
 }
