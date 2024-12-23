1e588b32cdea4c0b624e969806a2c8f0f7b9b839
imdla <imdla918@gmail.com>
2024-12-23 22:39:56 +0900

    [add] EtcProduct 클래스 및 생성자 생성

---
+++ src/main/java/org/example/EtcProduct.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class EtcProduct extends Product {
+
+    public EtcProduct(String name, int price, int stock) {
+        super(name, price, stock, Type.ETC);
+    }
+}
