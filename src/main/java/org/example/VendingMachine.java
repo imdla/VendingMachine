9e8a1afbc2e945a083630a93bbcf063b1551edbc
imdla <imdla918@gmail.com>
2024-12-23 22:47:24 +0900

    [modify] VendingMachine 변수 및 생성자 추가, 제품 추가 기능

--- src/main/java/org/example/VendingMachine.java
+++ src/main/java/org/example/VendingMachine.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VendingMachine {
+    public List<Product> productList;
+
+    public VendingMachine() {
+        this.productList = new ArrayList<>();
+    }
+
+    // 제품 추가
+    public List<Product> addProduct(Product product) {
+        productList.add(product);
+        return productList;
+    }
 }
