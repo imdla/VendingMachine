e0b7f5f8df4c4e85c8b44281031c9604bd4fb98d
imdla <imdla918@gmail.com>
2024-12-23 22:37:04 +0900

    [add] Beverage 클래스 생성

---
+++ src/main/java/org/example/Beverage.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Beverage extends Product {
+    public Beverage(String name, int price, int stock, Type type) {
+        super(name, price, stock, type);
+    }
+}
