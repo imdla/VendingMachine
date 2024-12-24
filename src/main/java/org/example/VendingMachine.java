d49e4f85c445c6182408f8382c7a88487cd517ce
imdla <imdla918@gmail.com>
2024-12-24 13:50:15 +0900

    [modify] VendingMachine 클래스 내 pay 메서드 내부 producList를 producMap으로 변경

--- src/main/java/org/example/VendingMachine.java
+++ src/main/java/org/example/VendingMachine.java
@@ -6,65 +6,49 @@ import java.util.List;
 import java.util.Map;
 
 public class VendingMachine {
-//    public List<Product> productList;
-    public Map<String, List<Integer>> productList;
+    public Map<String, List<Integer>> productMap;
     public Map<String, Integer> productSales;
 
     public VendingMachine() {
-//        this.productList = new ArrayList<>();
-        this.productList = new HashMap<>();
+        this.productMap = new HashMap<>();
         this.productSales = new HashMap<>();
     }
 
-//    // 제품 추가
-//    public List<Product> addProduct(Product product) {
-//        productList.add(product);
-//        productSales.put(product.name, 0);
-//        return productList;
-//    }
-//
-//    // 결제
-//    public void pay(String productName, Wallet wallet) {
-//        String paymentType = String.valueOf(wallet.type);
-//        Paymentable paymentable = null;
-//        int productPrice = 0;
-//
-//        for (Product product : productList) {
-//            if (product.name.equals(productName)) {
-//                productPrice = product.price;
-//            }
-//        }
-//
-//        // 결제
-//        switch (paymentType) {
-//            case "CASH":
-//                paymentable = new CashPayment();
-//                break;
-//            case "CARD":
-//                paymentable = new CardPayment();
-//                break;
-//        }
-//        paymentable.pay(wallet, productPrice);
-//
-//        // 재고 업데이트
-//        for (Product product : productList) {
-//            if (product.name.equals(productName)) {
-//                product.stock = product.stock - 1;
-//            }
-//        }
-//
-//        // 매출 업데이트
-//        int amount = productSales.getOrDefault(productName, 0);
-//        productSales.put(productName, amount + 1);
-//    }
-//
-//    // 제품명 검사
-//    public boolean validationProductName(String productName) {
-//        for (Product product : productList) {
-//            if (product.name.equals(productName)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
+    // 제품 추가
+    public Map<String, List<Integer>> putProduct(Product product) {
+        List<Integer> productList = new ArrayList<>();
+        productList.add(product.price);
+        productList.add(product.stock);
+
+        productMap.put(product.name, productList);
+        productSales.put(product.name, 0);
+        return productMap;
+    }
+
+    // 결제
+    public void pay(String productName, Wallet wallet) {
+        String paymentType = String.valueOf(wallet.type);
+        Paymentable paymentable = null;
+
+        // 금액 및 재고 확인, 재고 업데이트
+        List<Integer> productList = productMap.get(productName);
+        int productPrice = productList.getFirst();
+        int productStock = productList.getLast();
+        productStock = productStock - 1;
+
+        // 결제
+        switch (paymentType) {
+            case "CASH":
+                paymentable = new CashPayment();
+                break;
+            case "CARD":
+                paymentable = new CardPayment();
+                break;
+        }
+        paymentable.pay(wallet, productPrice);
+
+        // 매출 업데이트
+        int amount = productSales.getOrDefault(productName, 0);
+        productSales.put(productName, amount + 1);
+    }
 }
