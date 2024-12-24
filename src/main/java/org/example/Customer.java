3f5f18cd904793abad7b5436a307d0580c2103ee
imdla <imdla918@gmail.com>
2024-12-24 09:25:07 +0900

    [add] README.md README 파일 생성

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -10,6 +10,6 @@ public class Customer extends User {
 
     // 상품 구매
     public void buy(String productName) {
-        vendingMachine.pay(productName, wallet);
+        vendingMachine.pay(productName, wallet );
     }
 }
