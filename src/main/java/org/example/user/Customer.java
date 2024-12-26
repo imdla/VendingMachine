5b631773f02760ec73212a4de845fee54ee1488e
imdla <imdla918@gmail.com>
2024-12-26 15:15:13 +0900

    [modify] Customer 클래스의 자판기 최소 금액보다 잔액이 큰지 검사 메서드 추가

--- src/main/java/org/example/user/Customer.java
+++ src/main/java/org/example/user/Customer.java
@@ -14,16 +14,36 @@ public class Customer extends User implements ProductNameValidatable {
 
     // 자판기 사용
     public void use(VendingMachine vendingMachine){
-        // 자판기 상품 보여주기
-        showProducts(vendingMachine);
+        // 내가 가진 금액 > 자판기 최소 금액 확인
+        if (validateProductsMinPrice(vendingMachine, wallet)) {
+            // 자판기 상품 보여주기
+            showProducts(vendingMachine);
 
-        // 메뉴 선택
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What kind of product are you going to buy ?");
-        String productName = scanner.next();
+            // 메뉴 선택
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What kind of product are you going to buy ?");
+            String productName = scanner.next();
 
-        // 상품 구매
-        buy(productName, vendingMachine);
+            // 상품 구매
+            buy(productName, vendingMachine);
+        } else {
+            System.out.println("You have a small balance !");
+        }
+    }
+
+    // 자판기 최소 금액보다 큰지 검사
+    public boolean validateProductsMinPrice(VendingMachine vendingMachine, Wallet wallet) {
+        int productMinPrice = 10000;
+        for (List<Integer> productList : vendingMachine.productMap.values()) {
+            int productPrice = productList.getFirst();
+            productMinPrice = Math.min(productMinPrice, productPrice);
+        }
+
+        if (productMinPrice > wallet.balance) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     // 자판기 상품 보여주기
